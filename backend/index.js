0f3d0e53f2374832cd4b265e9a5782aaa83ef14c
dass300 <das300@gmail.com>
2022-11-01 23:20:03 +0530

    back end added

--- backend/index.js
+++ backend/index.js
@@ -10,3 +10,6 @@ app.use(bodyParser.urlencoded({ limit :"30mb",extended :true}))
 app.use(cors())
 
 const CONNECTION_URL = "mongodb+srv://dazzo:12345@cluster0.5rrvaai.mongodb.net/?retryWrites=true&w=majority"
+
+
+

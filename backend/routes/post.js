9d50081f75cfa135dfb1ecd64a6e42c73ecad55a
dass300 <das300@gmail.com>
2022-12-06 00:15:54 +0530

    modi

--- backend/routes/post.js
+++ backend/routes/post.js
@@ -1,9 +1,15 @@
 import express from 'express'
+import { getPosts,createPosts } from '../controllers/post.js'
 const router = express.Router()
 
-router.get('/',(req,res)=>{
-    res.send('THIS WORKS!')
-})
+router.get('/',getPosts)
+router.post('/',createPosts)
+
+
+
+
+
+
 
 export default router
 
